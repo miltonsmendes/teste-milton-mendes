ef9161edb86807813f283c6621adba84ec7d822c
unknown <miltonsergiom@gmail.com>
2024-08-26 01:04:43 -0400

    feat: addded recent equipment recent positions

--- desafio-aiko/src/components/Maps/index.jsx
+++ desafio-aiko/src/components/Maps/index.jsx
@@ -1,5 +1,17 @@
-// import L from "leaflet";
+import { useSelectedEquipament } from '../../hook/useSelectedEquipament';
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
+import equipmentPositionHistory from "../../../../data/equipmentPositionHistory.json";
+import equipmentStateHistory from "../../../../data/equipmentStateHistory.json";
+
+import "./styles.css";
+import "leaflet/dist/leaflet.css";
+
+export function Maps() {
+
+    const { selectedEquipament, isHistoryOn, isUpdatedPositionOn } = useSelectedEquipament();
+
+    const equipmentFiltered = equipmentPositionHistory.find(equipment => equipment.equipmentId === selectedEquipament);
 
     // const redIcon = new L.DivIcon({
     //     className: 'marker-icon-red marker-icon',
@@ -19,42 +31,92 @@
     //     iconAnchor: [12.5, 41],
     // });
 
-
-import { useSelectedEquipament } from '../../hook/useSelectedEquipament';
-
-import "./styles.css";
-import "leaflet/dist/leaflet.css";
-
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
-
-import equipmentPositionHistory from "../../../../data/equipmentPositionHistory.json";
-
-
-export function Maps() {
-
-    const { selectedEquipament, isHistoryOn, isUpdatedPositionOn } = useSelectedEquipament();
-
-    const equipmentFiltered = equipmentPositionHistory.find(equipment => equipment.equipmentId === selectedEquipament);
-
-
-    const findMostRecentPosition = (positions) => {
-        const mostRecentPosition = positions.reduce((mostRecent, current) => {
-            return new Date(current.date) > new Date(mostRecent.date) ? current : mostRecent;
-        });
+    // function findMostRecentPosition(positions) {
+    //     const mostRecentPosition = positions.reduce((mostRecent, current) => {
+    //         return new Date(current.date) > new Date(mostRecent.date) ? current : mostRecent;
+    //     });
     
-        return mostRecentPosition;
-    };
+    //     return mostRecentPosition;
+    // };
 
-    const mostRecentPositions = equipmentPositionHistory.map(equipment => {
-        const mostRecentPosition = findMostRecentPosition(equipment.positions);
+
+    // function findMostRecentStatus(positions) {
+    //     const mostRecentsStatus = positions.reduce((mostRecent, current) => {
+    //         return new Date(current.date) > new Date(mostRecent.date) ? current : mostRecent;
+    //     });
+    
+    //     return mostRecentsStatus;
+    // };
+
+    // function setState(id){
+    //     if (id === "0808344c-454b-4c36-89e8-d7687e692d57"){
+    //         return {
+    //             description: "Operando",
+    //             color: "greenIcon"
+    //         }
+    //     }
+    //     if (id === "baff9783-84e8-4e01-874b-6fd743b875ad"){
+    //         return {
+    //             description: "Parado",
+    //             color: "yellowIcon"
+    //         }
+    //     }
+    //     return {
+    //         description: "Manutenção",
+    //         color: "redIcon"
+    //     }
+    // }
+
+    // const mostRecentStatus = equipmentStateHistory.map(equipment => {
+    //     console.log("ola: ", equipment)
+    //     const mostRecentPosition = findMostRecentPosition(equipment.states);
+    //     const status = setState(equipment.equipmentId);
+        
+    //     return {
+    //         equipmentId: equipment.equipmentId,
+    //         position: mostRecentPosition,
+    //         status: {
+    //             id:equipment.equipmentId,
+    //             name: status.description,
+    //             color: status.color,
+    //         }
+    //     };
+    // });
+
+
+    let mostRecentStatuses = equipmentStateHistory.map(equipmentState => {
+        let mostRecentDate = null;
+        let position = null;
+    
+        const matchingPositionHistory = equipmentPositionHistory.find(equipmentPosition => 
+            equipmentState.equipmentId === equipmentPosition.equipmentId
+        );
+    
+        if (matchingPositionHistory) {
+
+            mostRecentDate = matchingPositionHistory.positions.reduce((mostRecent, current) => {
+                return new Date(current.date) > new Date(mostRecent.date) ? current : mostRecent;
+            }).date; 
+    
+            const result = matchingPositionHistory.positions.find(({ date }) => date === mostRecentDate);
+            
+            if (result) {
+                position = [result.lat, result.lon];
+            }
+        }
+    
         return {
-            equipmentId: equipment.equipmentId,
-            position: mostRecentPosition,
+            id: equipmentState.equipmentId,
+            date: mostRecentDate,
+            position,
         };
     });
-
-
-    return (
+    
+    console.log(mostRecentStatuses);
+    
+    
+    
+    return ( 
         <>
             <MapContainer center={[-19.03822, -45.856232]} zoom={13}>
                 <TileLayer
@@ -77,15 +139,12 @@ export function Maps() {
                 {
                     isUpdatedPositionOn 
                     ?
-                    mostRecentPositions.map((equipment, index) => (
+                    mostRecentStatuses.map((equipment, index) => (
                         <Marker 
                             key={index} 
-                            position={[equipment.position.lat, equipment.position.lon]} 
-                            // Caso queira usar um ícone personalizado, descomente as linhas abaixo:
-                            // icon={redIcon} 
+                            position={equipment.position}
                         />
                     ))
-                    
                     : <></>
                 }
             </MapContainer>
